6d05f83164da834bb906432e42b0140d516b9028
52pig <1040978476@qq.com>
2024-10-07 23:37:17 +0800

    dragon_v1_add_buy_fix

--- strategy/dragon_v1.py
+++ strategy/dragon_v1.py
@@ -122,34 +122,40 @@ class Dragon_V1(BaseStrategy):
             mid_start_time = datetime.datetime.strptime("11:30", "%H:%M").time()
             mid_end_time = datetime.datetime.strptime("13:01", "%H:%M").time()
             end_time = datetime.datetime.strptime("14:55", "%H:%M").time()
-
+            is_trade_time = start_time <= cur_time <= mid_start_time or mid_end_time <= cur_time <= end_time
             # 挂隔夜单买入
             action_name = ''
             gy_order_id = ''
             current_price = utils.get_latest_price(stock_code)
             order_id = -1
-            if gy_time <= cur_time or cur_time >= start_time:
+            if gy_time <= cur_time or is_trade_time:
                 ## 买入委托
                 action_name = "buy"
                 # 查询账户余额
                 acc_info = xt_trader.query_stock_asset(acc)
                 cash = acc_info.cash
-                ## 若账户余额> 股票价格*100，则买入
-                # print(action_name, current_price)
-                # 下单
-                if current_price is not None:
-                    if cash >= current_price * 100:
-                        order_id = xt_trader.order_stock(acc, stock_code, xtconstant.STOCK_BUY, 100,
-                                                         xtconstant.FIX_PRICE, current_price)
+
+
+                has_stock_list = xt_trader.query_stock_positions(acc)
+                for stock in has_stock_list:
+                    if stock.stock_code != stock_code:
+                        continue
+                    ## 若账户余额> 股票价格*100，则买入 并且只有1手
+                    # print(action_name, current_price)
+                    # 下单
+                    if current_price is not None:
+                        if cash >= current_price * 100 and stock.volume < 200:
+                            order_id = xt_trader.order_stock(acc, stock_code, xtconstant.STOCK_BUY, 100,
+                                                             xtconstant.FIX_PRICE, current_price)
             elif cur_time == jj_time:
                 ## 检查该股票的封单量是否相同连板数中最高
                 is_highest = is_highest_bid(stock_code)
                 if not is_highest:
                     action_name = 'cancel'
                     ## 取消买入委托的订单
-                    xt_trader.cancel_order_stock(acc, gy_order_id)
+                    xt_trader.cancel_order_stock(acc, order_id)
 
-            elif start_time <= cur_time <= mid_start_time or mid_end_time <= cur_time <= end_time:
+            elif is_trade_time:
                 # 检查当前时间是否在 9:30 到 15:00 之间
                 #1，每分钟监测股价走势，若股价 < 前一日收盘价则卖出；
                 #2，每分钟监测股价走势，若股价 > 前一日收盘价，计算(股价 - 前一日收盘价) / 前一日收盘价 > 5 % 则持有观察，若 < 5 % 则以当前价格卖出。
